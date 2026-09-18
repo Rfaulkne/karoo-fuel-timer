5b9526ed5d1cba1c9dd33eea197ec4f0b832a478
Rfaulkne <faulkner@is.mpg.de>
2026-09-18 12:01:19 +0200

    Bump Fuel Timer to v0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rfaulkne.karoofueltimer3"
         minSdk = 26
         targetSdk = 29
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
     }
 
     buildTypes {
